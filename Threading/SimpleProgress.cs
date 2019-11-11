d086806d3d411d399755a970c948f33df3eedc96
Jerry Kim <jerryrox@live.co.kr>
2019-11-11 15:28:34 +1300

    Added SimpleProgress for basic IProgress usage

---
+++ Threading/SimpleProgress.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PBFramework.Threading
+{
+    /// <summary>
+    /// A basic implementation of IProgress interface.
+    /// </summary>
+    public class SimpleProgress : IProgress<float> {
+
+        public float Progress { get; set; }
+
+
+        public void Report(float progress) => Progress = progress;
+    }
+}
