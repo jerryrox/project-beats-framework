02d6e5a60a18e568b445f648b9e27a9802509e2d
Jerry Kim <jerryrox@live.co.kr>
2019-12-11 20:24:41 +0900

    Initial ILabel implementation

--- Graphics/UI/ILabel.cs
+++ Graphics/UI/ILabel.cs
@@ -1,18 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using PBFramework.Assets.Fonts;
 
-public class ILabel : MonoBehaviour
+namespace PBFramework.Graphics.UI
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public interface ILabel : IGraphicObject, IHasColor {
         
-    }
+        /// <summary>
+        /// The font this label will use for rendering the text.
+        /// </summary>
+        IFont Font { get; set; }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        /// <summary>
+        /// Whether the text should be rendered using a bold font.
+        /// Default: false
+        /// </summary>
+        bool IsBold { get; set; }
+
+        /// <summary>
+        /// Whether the text should be displayed using italics.
+        /// Default: false
+        /// </summary>
+        bool IsItalic { get; set; }
+
+        /// <summary>
+        /// Whether the text should be wrapped within the size width and height.
+        /// Default: false.
+        /// </summary>
+        bool WrapText { get; set; }
+
+        /// <summary>
+        /// Size of the displayed text.
+        /// Default: 20
+        /// </summary>
+        int FontSize { get; set; }
+
+        /// <summary>
+        /// The pivot point which the text will be aligned from.
+        /// Default: UpperLeft
+        /// </summary>
+        TextAnchor Alignment { get; set; }
+
+        /// <summary>
+        /// The actual text value to be displayed on the label.
+        /// </summary>
+        string Text { get; set; }
     }
 }
