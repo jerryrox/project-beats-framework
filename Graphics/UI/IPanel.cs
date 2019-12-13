235d131d33e2587b6361aa1c2c2cf4e4b3071c0c
Jerry Kim <jerryrox@live.co.kr>
2019-12-13 19:23:13 +0900

    Initial IPanel implementation

--- Graphics/UI/IPanel.cs
+++ Graphics/UI/IPanel.cs
@@ -1,18 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class IPanel : MonoBehaviour
+namespace PBFramework.Graphics.UI
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public interface IPanel : IGraphicObject, IHasAlpha, IHasMask {
+    
+        /// <summary>
+        /// The sprite which the masking will be based on.
+        /// </summary>
+        Sprite MaskSprite { get; set; }
     }
 }
