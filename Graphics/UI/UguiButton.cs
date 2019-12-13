83e2f91846335f99b00cdf34d08c35ae10a3d0ee
Jerry Kim <jerryrox@live.co.kr>
2019-12-13 16:20:05 +0900

    Update UguiButton.cs

--- Graphics/UI/UguiButton.cs
+++ Graphics/UI/UguiButton.cs
@@ -33,45 +33,15 @@ namespace PBFramework.Graphics.UI
             SetNoTransition();
         }
 
-        public void SetNoTransition()
-        {
-            button.transition = Selectable.Transition.None;
-        }
+        public void SetNoTransition() => button.SetNoTransition();
 
         public void SetSpriteSwapTransition(Sprite highlight, Sprite selected, Sprite pressed, Sprite disabled)
-        {
-            button.transition = Selectable.Transition.SpriteSwap;
-            button.spriteState = new SpriteState()
-            {
-                highlightedSprite = highlight,
-                selectedSprite = selected,
-                pressedSprite = pressed,
-                disabledSprite = disabled
-            };
-        }
+            => button.SetSpriteSwapTransition(highlight, selected, pressed, disabled);
 
         public void SetColorTintTransition(Color normal, Color highlight, Color selected, Color pressed, Color disabled, float duration)
-        {
-            button.transition = Selectable.Transition.ColorTint;
-            button.colors = new ColorBlock()
-            {
-                normalColor = normal,
-                highlightedColor = highlight,
-                selectedColor = selected,
-                pressedColor = pressed,
-                disabledColor = disabled,
-            };
-        }
+            => button.SetColorTintTransition(normal, highlight, selected, pressed, disabled, duration);
 
         public void SetColorTintTransition(Color normal, float duration)
-        {
-            Color highlight = normal * 1.05f;
-            Color selected = normal * 0.95f;
-            Color pressed = normal * 0.9f;
-            Color disabled = normal * 0.5f;
-            highlight.a = selected.a = pressed.a = disabled.a = normal.a;
-
-            SetColorTintTransition(normal, highlight, selected, pressed, disabled, duration);
-        }
+            => button.SetColorTintTransition(normal, duration);
     }
 }
