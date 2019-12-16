6695509495e01703dea49bcf61096365124de37e
Jerry Kim <jerryrox@live.co.kr>
2019-12-16 18:42:13 +0900

    Initial ISlider implementation

--- Graphics/UI/ISlider.cs
+++ Graphics/UI/ISlider.cs
@@ -1,18 +1,55 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System;
+using UnityEngine.UI;
 
-public class ISlider : MonoBehaviour
+namespace PBFramework.Graphics.UI
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public interface ISlider : IGraphicObject, IHasTransition {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        /// <summary>
+        /// Event called when the scrollbar value has changed.
+        /// </summary>
+        event Action<float> OnChange;
+
+
+        /// <summary>
+        /// Returns the background sprite of the slider.
+        /// </summary>
+        ISprite Background { get; }
+
+        /// <summary>
+        /// Returns the foreground sprite of the slider.
+        /// </summary>
+        ISprite Foreground { get; }
+
+        /// <summary>
+        /// Returns the thumb sprite of the slider.
+        /// </summary>
+        ISprite Thumb { get; }
+
+        /// <summary>
+        /// The direction of the slider movement.
+        /// Default: LeftToRight
+        /// </summary>
+        Slider.Direction Direction { get; set; }
+
+        /// <summary>
+        /// Whether the slider value should move on whole numbers only.
+        /// </summary>
+        bool IsWholeNumber { get; set; }
+
+        /// <summary>
+        /// The minimum value of the slider scale.
+        /// </summary>
+        float MinValue { get; set; }
+
+        /// <summary>
+        /// The maximum value of the slider scale.
+        /// </summary>
+        float MaxValue { get; set; }
+
+        /// <summary>
+        /// Current slider value.
+        /// </summary>
+        float Value { get; set; }
     }
 }
