39af3a3a52cc0060e70cb6cabe5f5cdaebbc8ad3
Jerry Kim <jerryrox@live.co.kr>
2019-12-16 18:58:33 +0900

    Initial IScrollView implementation

--- Graphics/UI/IScrollView.cs
+++ Graphics/UI/IScrollView.cs
@@ -1,18 +1,68 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class IScrollView : MonoBehaviour
+namespace PBFramework.Graphics.UI
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public interface IScrollView : IGraphicObject, IHasMask, IHasAlpha {
+    
+        /// <summary>
+        /// Whether the scrollview can be scrolled horizontally.
+        /// Default: false.
+        /// </summary>
+        bool IsHorizontal { get; set; }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        /// <summary>
+        /// Whether the scrollview can be scrolled vertically.
+        /// Default: true.
+        /// </summary>
+        bool IsVertical { get; set; }
+
+        /// <summary>
+        /// Whether inertia effect should be used.
+        /// Default: true
+        /// </summary>
+        bool UseInertia { get; set; }
+
+        /// <summary>
+        /// If supports inertia, how much is the application rate. 
+        /// Default: 0.1
+        /// </summary>
+        float InertiaRate { get; set; }
+
+        /// <summary>
+        /// Amount of elasticity applied to scrolling when movement supports elastic mode.
+        /// </summary>
+        float Elasticity { get; set; }
+
+        /// <summary>
+        /// The background sprite of the scrollview.
+        /// </summary>
+        ISprite Background { get; }
+
+        /// <summary>
+        /// The type of movement effect applied on the scrollview.
+        /// </summary>
+        ScrollRect.MovementType Movement { get; set; }
+
+        /// <summary>
+        /// Scrollbar for horizontal scrolling.
+        /// </summary>
+        IScrollBar HorizontalScrollbar { get; set; }
+
+        /// <summary>
+        /// Scrollbar for vertical scrolling.
+        /// </summary>
+        IScrollBar VerticalScrollbar { get; set; }
+
+        /// <summary>
+        /// Returns the scrollview content container object.
+        /// </summary>
+        IGraphicObject Container { get; }
+
+
+        /// <summary>
+        /// Positions the container to specified position.
+        /// </summary>
+        void ScrollTo(Vector2 position);
     }
 }
