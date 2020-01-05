68a86b5c3a2cf158db6ee99f21f0a7c902ed3bab
Jerry Kim <jerryrox@live.co.kr>
2020-01-05 15:38:30 +0900

    Implemented audio requesters to support audio interfacing

---
+++ Networking/EffectAudioRequest.cs
@@ -0,0 +1,27 @@
+using PBFramework.Audio;
+
+namespace PBFramework.Networking
+{
+    public class EffectAudioRequest : ProxyPromise<IEffectAudio> {
+
+        /// <summary>
+        /// The inner request being wrapped over.
+        /// </summary>
+        private AudioRequest audioRequest;
+
+
+        public EffectAudioRequest(string url, bool isStream = false) : base()
+        {
+            // Create the actual request to make.
+            audioRequest = new AudioRequest(url, isStream);
+
+            // Bind proxied action.
+            startAction = audioRequest.Start;
+            revokeAction = audioRequest.Revoke;
+
+            // Hook onto the request.
+            audioRequest.OnProgress += SetProgress;
+            audioRequest.OnFinishedResult += (audio) => Resolve(new UnityAudio(audio));
+        }
+    }
+}
---
+++ Networking/MusicAudioRequest.cs
@@ -0,0 +1,27 @@
+using PBFramework.Audio;
+
+namespace PBFramework.Networking
+{
+    public class MusicAudioRequest : ProxyPromise<IMusicAudio> {
+
+        /// <summary>
+        /// The inner request being wrapped over.
+        /// </summary>
+        private AudioRequest audioRequest;
+
+
+        public MusicAudioRequest(string url, bool isStream = true) : base()
+        {
+            // Create the actual request to make.
+            audioRequest = new AudioRequest(url, isStream);
+
+            // Bind proxied action.
+            startAction = audioRequest.Start;
+            revokeAction = audioRequest.Revoke;
+
+            // Hook onto the request.
+            audioRequest.OnProgress += SetProgress;
+            audioRequest.OnFinishedResult += (audio) => Resolve(new UnityAudio(audio));
+        }
+    }
+}
