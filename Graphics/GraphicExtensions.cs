8d4326fb40c972665239f295f4e6f3d92930f559
Jerry Kim <jerryrox@live.co.kr>
2020-04-22 20:32:05 +1200

    Update GraphicExtensions.cs

--- Graphics/GraphicExtensions.cs
+++ Graphics/GraphicExtensions.cs
@@ -1,7 +1,11 @@
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace PBFramework.Graphics
 {
+    /// <summary>
+    /// Extension class which provides extra functions in a graphical context.
+    /// </summary>
     public static class GraphicExtensions {
 
         public static void SetColorR(this Graphic context, float r)
@@ -30,6 +34,24 @@ namespace PBFramework.Graphics
             var color = context.color;
             color.a = a;
             context.color = color;
+        }
+
+        /// <summary>
+        /// Sets this color's RGB values to the specified tint without touching alpha.
+        /// </summary>
+        public static void SetTint(this Color context, Color tint)
+        {
+            context.r = tint.r;
+            context.g = tint.g;
+            context.b = tint.b;
+        }
+
+        /// <summary>
+        /// Returns a new vector translated by the specified delta.
+        /// </summary>
+        public static Vector2 Translated(this Vector2 context, float x, float y)
+        {
+            return new Vector2(context.x + x, context.y + y);
         }
     }
 }
