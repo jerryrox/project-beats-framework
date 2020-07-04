701010b43f6e0cda44b89156256552c0ba6bbeaa
Jerry Kim <jerryrox@live.co.kr>
2020-07-04 15:08:09 +1200

    Exposed deep link creator method for platform host.

--- Platforms/IPlatformHost.cs
+++ Platforms/IPlatformHost.cs
@@ -1,10 +1,15 @@
+using PBFramework.Networking.Linking;
+
 namespace PBFramework.Platforms
 {
     /// <summary>
     /// Interface of a platform-specific functionality provider based on different hosts.
     /// </summary>
     public interface IPlatformHost {
-    
-        
+
+        /// <summary>
+        /// Creates a new deeplinker instance.
+        /// </summary>
+        DeepLinker CreateDeepLinker();
     }
 }
